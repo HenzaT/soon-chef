b2a713d3f841b82db1d63cec7c578b10ff27d936
Henry Thomas <henrykun95@gmail.com>
2025-09-12 16:57:15 +0100

    added nokogiri logic

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,7 @@
+require 'nokogiri'
+require 'json'
+require 'open-uri'
+
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
@@ -8,8 +12,18 @@ class RecipesController < ApplicationController
   end
 
   def new
+    nokogiri_recipe
   end
 
   def create
+  end
+
+  private
+
+  def nokogiri_recipe
+    url = 'https://lianaskitchen.co.uk/easy-chicken-soup-recipe/'
+    html = URI.open(url)
+    doc = Nokogiri::HTML.parse(html)
+    @scripts = doc.css('script[type="application/ld+json"]')
   end
 end
