c52766b8d149c536e1d28f55848eed14f16096da
Henry Thomas <henrykun95@gmail.com>
2025-09-25 21:24:57 +0100

    routing user routes, added names to user model

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,10 @@
 module RecipesHelper
+  include Devise::Controllers::Helpers
+  def logout_of_session
+    link_to 'Logout', destroy_user_session_path, data: { turbo_method: :delete } if user_signed_in?
+  end
+
+  def welcome_user
+    "Hi #{current_user.first_name}" if user_signed_in?
+  end
 end
